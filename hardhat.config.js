c3b05c3b97f6de1615cb0cdc4369de99a7542552
Dev Pratap Singh <devthakur31931@gmail.com>
2024-02-02 13:51:29 +0530

    ERC20

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,20 +1,28 @@
 /**
-* @type import('hardhat/config').HardhatUserConfig
-*/
+ * @type import('hardhat/config').HardhatUserConfig
+ */
 
 require('dotenv').config();
 require("@nomiclabs/hardhat-ethers");
 
 const { API_URL, PRIVATE_KEY } = process.env;
 
+if (!API_URL || !PRIVATE_KEY) {
+  throw new Error("Please provide both API_URL and PRIVATE_KEY in your .env file.");
+}
+
+if (PRIVATE_KEY.length !== 64) {
+  throw new Error("Private key length is invalid. It should be a 64-character hexadecimal string.");
+}
+
 module.exports = {
    solidity: "0.8.4",
-   defaultNetwork: "goerli",
+   defaultNetwork: "sepolia",
    networks: {
       hardhat: {},
-      goerli: {
+      sepolia: {
          url: API_URL,
-         accounts: [`0x${PRIVATE_KEY}`]
+         accounts: [PRIVATE_KEY],
       }
    },
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,30 +1,22 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+// import ethers from "ethers";
+// const{ethers} = require("ethers");
+async function main(){
+  const [deployer] = await ethers.getSigners();
+  console.log("Deploying contract with account ", deployer.address);
+ const bal =  (await deployer.getBalance()).toString();
+//  const Bal = await utils.formatEther(bal);
+//  console.log(utils.formatEther(bal));
+//  console.log("Account Balance is : ",Bal);
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+//  console.log("Account Balance is : ",(await deployer.getBalance()).toString());
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  const Temp = await ethers.getContractFactory("GovernanceToken");
+  const temp = await Temp.deploy();
+  console.log("contract address is ",temp.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
+
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
